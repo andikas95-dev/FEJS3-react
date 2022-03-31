5534c0a2b2f96ac6b7ef715bf39f9e2b7b1b683a
andikas95-dev <andikaperdana95@gmail.com>
2022-03-31 21:00:48 +0700

    Belajar React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,149 +1,26 @@
-// import Hello from './Hello';
-// import Header from './components/Header/Header';
-import React, { useState } from 'react';
-import { Button, Col, Row } from 'reactstrap';
-import { Header, NewComponent } from './components/Header/Header';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Layout from './components/Layouts/Layout';
+import About from './pages/About/About';
+import NotFound from './components/NotFound/NotFound';
+
 import './App.css';
 
-// function NewComponent() {
-//   return <div>ini New Component</div>;
-// }
-
 function App() {
-  // const name = 'Nyoman';
-
-  const [name, setName] = useState('Nyoman');
-  const [alamat, setAlamat] = useState('Bali');
-  const [umur] = useState(23);
-
-  const [loginData, setLoginData] = useState({
-    email: null,
-    password: null,
-  });
-
-  const changeNama = (event) => {
-    console.log(event);
-    setName(event.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(e);
-  };
-
   return (
-    <>
-      {/* <div className="App">{name}</div>
-      <input
-        type="text"
-        value={name}
-        onChange={(event) => {
-          console.log(event);
-          setName(event.target.value);
-        }}
-      /> */}
-      <Header
-        nama={name}
-        alamat={alamat}
-        umur={26}
-        gantiNama={(e) => setName(e.target.value)}
-        text={<h1>Ini tg html di dalam property/attribute</h1>}
-      />
-      <NewComponent />
-      {/* <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      /> */}
-      <h1>Hello {name}</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          value={loginData.email}
-          onChange={(e) =>
-            setLoginData((prev) => ({ ...prev, email: e.target.value }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={loginData.password}
-          onChange={(e) =>
-            setLoginData((prev) => ({ ...prev, password: e.target.value }))
-          }
-        />
-        <Button type="submit" color="primary" outline>
-          submit
-        </Button>
-      </form>
-      {/* <button onClick={() => changeNama()}>change name</button> */}
-      <Row>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-        <Col md={1} style={{ border: '1px solid tomato' }}>
-          <p>Test</p>
-        </Col>
-      </Row>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+
+        {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: 'Nyoman',
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <div className="App">{this.state.name}</div>
-//         <input
-//           type="text"
-//           value={this.state.name}
-//           onChange={(event) => this.setState({ name: event.target.value })}
-//         />
-//       </>
-//     );
-//   }
-// }
 
 export default App;
