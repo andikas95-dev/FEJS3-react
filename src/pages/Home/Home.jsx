2a162faae951dc39300dd56e216718c1ea9c156f
andikas95-dev <andikaperdana95@gmail.com>
2022-04-01 16:09:00 +0700

    new axios create and get

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,105 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  const [data, setData] = React.useState([]);
+  const [newData, setNewData] = React.useState({
+    nama: '',
+    alamat: '',
+  });
+
+  useEffect(() => {
+    getDataTodos();
+  }, []);
+
+  /**
+   * Get data from API with then catch
+   */
+  // const getDataTodos = () => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => res.json())
+  //     .then((json) => setData(json))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // const getDataTodos = () => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => setData(res.data))
+  //     .catch((err) => console.log(err));
+  // };
+
+  /**
+   * Get data from API with async/await
+   */
+
+  // const getDataTodos = async () => {
+  //   try {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  //     const json = await res.json();
+  //     setData(json);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const getDataTodos = async () => {
+    try {
+      const res = await axios.get('http://localhost:3000/tamu');
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const createDataPost = async () => {
+    try {
+      await axios.post('http://localhost:3000/tamu', newData);
+      getDataTodos();
+      setNewData({
+        nama: '',
+        alamat: '',
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(newData);
+
+  return (
+    <div>
+      <input
+        placeholder="Nama Tamu"
+        value={newData.nama}
+        onChange={(e) =>
+          setNewData((prev) => ({
+            ...prev,
+            nama: e.target.value,
+          }))
+        }
+      />
+      <input
+        placeholder="Alamat Tamu"
+        value={newData.alamat}
+        onChange={(e) =>
+          setNewData((prev) => ({
+            ...prev,
+            alamat: e.target.value,
+          }))
+        }
+      />
+      <button onClick={() => createDataPost()}>Create</button>
+
+      {data.length > 0 &&
+        data.map((item) => {
+          return (
+            <div key={item.id}>
+              <h1>Nama Tamu: {item.nama}</h1>
+              <p>Alamat Rumah: {item.alamat}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Home;
